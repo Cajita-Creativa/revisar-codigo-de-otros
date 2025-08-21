f3ad45f130878eaf9569c561be0abea855a9b1ef
Stefani Cosío <stefanicosio@hotmail.com>
2025-08-21 12:05:16 -0600

    Se hace correccion a funcion asincrona, se declaran variables correctamente

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,25 @@ const $n = document.querySelector('.name'); // no existe una etiqueta name, es u
 const $b = document.querySelector('.blog'); // aqui le hacía referencia a un id y es clase en el html
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+//Le hace falta la funcion asyncrona con el try y catch
+async function displayUser(username) {
+  try {
+      $n.textContent = 'cargando...';
+      const response = await fetch(`${usersEndpoint}/${username}`);
+      const data = await response.json(); //se pide data y no estaba declarada
+      console.log(data);
+      $n.textContent = '${data.name}';
+      $b.textContent = '${data.blog}';
+      $l.textContent = '${data.location}';
+    } catch (err) {
+    handleError(err);
+  }
 }
 
-function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
-}
+    function handleError(err) {
+      console.log('OH NO!');
+      console.log(err);
+      $n.textContent = `Algo salió mal: ${err}` //se comio el $ de la constante
+    }
 
-displayUser('stolinski').catch(handleError);
+  displayUser('stolinski').catch(handleError);
